040671189138720499e3d8eaa5fffbe9d65fbe5c
Aleksey <mc_gra_dy@mail.ru>
2023-04-18 18:59:19 +0300

    переделал тест test_apply_discount

--- test_main.py
+++ test_main.py
@@ -4,11 +4,12 @@ def test_get_total_price():
     product1 = Product("Телевизор", 30000, 10)
     assert product1.get_total_price() == 300000
 
-'''
+
 def test_apply_discount():
     product2 = Product("Холодильник", 25000, 5)
-    assert product2.apply_discount(0.1) == 112500
-'''
+    product2.apply_discount(0.1)
+    assert product2.get_total_price()
+
 
 
 def test_get_total_inventory_value():
