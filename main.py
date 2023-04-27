71e6555b97d321f4eab106d1ddcbf5e4e18dbbb3
Aleksey <mc_gra_dy@mail.ru>
2023-04-27 19:09:31 +0300

    выполнил 2 дз первое задание

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ class Product:
     self.quantity = quantity
     self.__class__.products.append(self)
 
+  @property
+  def name(self):
+      return self._name
+
+  @name.setter
+  def name(self, value):
+      if len(value) <= 10:
+          self._name = value
+      else:
+          print('Длина наименования товара превышает 10 символов.')
+
+  @classmethod
+  def instantiate_from_csv(cls):
+      with open('src/item.csv', 'r') as f:
+          for line in f:
+              name, price, quantity = line.strip().split(',')
+              item = cls(name, price, quantity)
+
+  @staticmethod
+  def string_to_number(string):
+      return float(string)
   def get_total_price(self):
     return self.quantity * self.price * self.price_level
 
@@ -23,7 +44,7 @@ class Product:
 if __name__ == "__main__":
 # Создаем экземпляры товаров
     product1 = Product("Телевизор", 30000, 10)
-    product2 = Product("Холодильник", 25000, 5)
+    product2 = Product("Холод", 25000, 5)
 
 # Получаем общую стоимость конкретного товара
     print(product1.get_total_price())
@@ -36,6 +57,27 @@ if __name__ == "__main__":
 
 # Получаем общую стоимость всего запаса товаров в магазине
     print(Product.get_total_inventory_value())
+
+    item = Product('Телефон', 10000, 5)
+
+    # длина наименования товара меньше 10 символов
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+
+    # длина наименования товара больше 10 символов
+    item.name = 'СуперСмартфон'
+    # Exception: Длина наименования товара превышает 10 символов.
+
+    Product.instantiate_from_csv()  # создание объектов из данных файла
+
+    assert len(Product.products) == 9  # в файле 9 записей с данными по товарам
+
+    item1 = Product.products[0]
+    assert item1.name != 'Смартфон'
+
+    assert Product.string_to_number('5') == 5
+    assert Product.string_to_number('5.0') == 5
+    assert Product.string_to_number('5.5') != 5
 
 
 
